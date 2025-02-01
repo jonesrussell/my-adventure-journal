0a35983eede32ca16efc9a1eabfab372a9d2b975
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2025-02-01 11:52:00 +0000

    cleanup

--- types/declarations.d.ts
+++
@@ -1,4 +0,0 @@
-// declare module '@eslint/js';
-
-// declare module 'eslint-plugin-react/configs/recommended.js';
-
--- types/global.d.ts
+++
@@ -1,9 +0,0 @@
-// interface ExtendedGlobal extends NodeJS.Global {
-//   mongoose?: {
-//     conn: typeof import('mongoose') | null;
-//     promise: Promise<typeof import('mongoose')> | null;
-//   };
-// }
-
-// // Then, adjust your declaration accordingly
-// declare const global: ExtendedGlobal;
