ef8a2f827c841843caeeb37f675fc3bdc9705a4e
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2025-02-02 20:06:55 +0000

    Configure MongoDB replica set and add initial data
    - Enable MongoDB replica set configuration in docker-compose
    - Add init-replica-set.js to initiate replica set
    - Update mongo-init.js with additional user and sample adventure data
    - Improve MongoDB initialization scripts for development environment

--- .devcontainer/mongo-init.js
+++ .devcontainer/mongo-init.js
@@ -1,16 +1,39 @@
 // Connect to the admin database and create the admin user
 db = db.getSiblingDB('admin');
 db.createUser({
-   user: "myadmin",
-   pwd: "foobar123my",
-   roles: [{ role: "root", db: "admin" }]
+  user: 'myadmin',
+  pwd: 'foobar123my',
+  roles: [{ role: 'root', db: 'admin' }]
 });
 
 // Connect to the "myjournal" database and create a user for it
 db = db.getSiblingDB('myjournal');
 db.createUser({
-   user: "journaluser",
-   pwd: "securepassword123",
-   roles: [{ role: "dbOwner", db: "myjournal" }]
+  user: 'journaluser',
+  pwd: 'securepassword123',
+  roles: [{ role: 'dbOwner', db: 'myjournal' }]
 });
+
+db.createUser({
+  user: 'myUserAdmin',
+  pwd: 'abc123',
+  roles: [
+    { role: 'readWrite', db: 'myjournal' },
+    { role: 'dbAdmin', db: 'myjournal' }
+  ]
+});
+
+db.createCollection('adventures');
+db.adventures.insertMany([
+  {
+    name: 'Adventure 1',
+    location: 'Location 1',
+    description: 'Description for Adventure 1'
+  },
+  {
+    name: 'Adventure 2',
+    location: 'Location 2',
+    description: 'Description for Adventure 2'
+  }
+]);
 
